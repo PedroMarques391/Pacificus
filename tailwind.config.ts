8cdfedee48d12f8e23425c1897510407b3d79f18
Pedro <pedromarques391@gmail.com>
2024-04-25 14:36:27 -0300

    feat: Finalizando a página principal do site

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,31 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "BannerHome" : 'url("/banners/BannerHome.png")',
+          "BannerArea" : 'url("/banners/BannerArea.png")',
+          "BannerTeam" : 'url("/banners/BannerTeam.png")',
+          "BannerContact" : 'url("/banners/BannerContact.png")',
+          "BannerEthics" : 'url("/banners/BannerEthics.png")',
       },
+      colors: {
+        "light-100": "#FFFFFF",
+        "light-200": "#808080 ",
+        "light-300": "#E0F2F1 ",
+        "light-400": "#F9F5E1 ",
+        "light-500": "#EBF5E1 ",
+        "light-600": "#FFF8DC",
+        "light-700": "#F0F8FF ",
+        "light-800": "#FFFFF0 ",
+      },
+      keyframes: {
+        line: {
+          "0%": {"width": "0"},
+          "100%": {"width": "100%"},
+        }
+      },
+      animation: {
+        lines: "line 1s ease-in-out forwards"
+      }
     },
   },
   plugins: [],
