f7e0458540ed31d69c226eee285f5e58d84e1cde
Pedro <pedromarques391@gmail.com>
2024-04-27 00:33:42 -0300

    fix: Corrigindo erros de responsividade e otimizando o carregamento de imagens.

--- src/components/team/Associate.tsx
+++ src/components/team/Associate.tsx
@@ -17,14 +17,14 @@ const Associate = ({ name, email, training, more }: IAssociateProps): React.JSX.
             <div className='flex justify-center items-center lg:gap-x-5'>
                 {IconUser}
                 <div>
-                    <H1 className='text-xl my-3 uppercase tracking-wider'>{name}</H1>
-                    <H1 className='text-lg my-3'>{email}</H1>
+                    <H1 className='text-base lg:text-xl my-3 uppercase lg:tracking-wider'>{name}</H1>
+                    <H1 className='text-sm lg:text-lg my-3'>{email}</H1>
                 </div>
             </div>
 
             <div className='w-full ml-5'>
-                <H1 className='text-xl text-black/100 text-start'>Formação</H1>
-                <ul className='flex text-lg flex-col gap-y-3 text-light-200 list-disc'>
+                <H1 className='text-base lg:text-xl text-black/100 text-start'>Formação</H1>
+                <ul className='flex lg:text-lg flex-col gap-y-3 text-light-200 list-disc'>
                     {training.map((training, index) => (
                         <li key={index}>
                             {training}
@@ -34,8 +34,8 @@ const Associate = ({ name, email, training, more }: IAssociateProps): React.JSX.
             </div>
 
             <div className='w-full ml-5'>
-                <H1 className='text-xl text-black/100 text-start'>Sobre o Profissional</H1>
-                <ul className='flex text-lg flex-col gap-y-3 text-light-200 list-disc'>
+                <H1 className='text-base lg:text-xl text-black/100 text-start'>Sobre o Profissional</H1>
+                <ul className='flex lg:text-lg flex-col gap-y-3 text-light-200 list-disc'>
                     {more.map((more, index) => (
                         <li key={index}>
                             {more}
